5c2533181ed293deb7fb1bdb03b5839625d3f713
Glitch (osde8info-node-express-cloudinary) <none>
2020-01-18 11:52:16 +0000

    🚘🚙 Updated with Glitch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv-safe').config()
 
 const express = require('express')
+
 const app = express()
 
 app.use(express.static('public'))
@@ -8,12 +9,23 @@ app.use(express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/', async (_, res) => {
-  const pugdata = {} // await getMaptiles()
+  const pugdata = {}
   console.info(new Date())
   res.render('index', pugdata)
 })
 
-const server = app.listen(process.env.PORT, () => {
-  const port = server.address().port
-  console.log(`App listening at http://localhost:${port}`)
-})
+function listenwrap() {
+  try {
+    const server = app.listen(process.env.PORT, () => {
+      const family = server.address().family
+      const host = process.env.HOST_ADDRESS
+      const port = server.address().port
+      console.info(new Date())
+      console.log(`App listening at ${family} http://${host}:${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+listenwrap()
