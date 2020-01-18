5140499ecd4ecf3b767f91d114c532bacf90fc71
Glitch (osde8info-node-express-cloudinary) <none>
2020-01-18 10:05:42 +0000

    ⏲👣 Updated with Glitch

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,31 +1,29 @@
 self.addEventListener('fetch', event => {
   if (/\.jpg$|.png$|.gif$|.webp$/.test(event.request.url)) {
-    const connection = navigator.connection.effectiveType;
-    let imageQuality;
+    const connection = navigator.connection.effectiveType
+    let imageQuality
     switch (connection) {
       case '4g':
-        imageQuality = 'q_auto:good';
-        break;
-      case '3g': 
-        imageQuality = 'q_auto:eco';
-        break;
-      case'2g':
+        imageQuality = 'q_auto:good'
+        break
+      case '3g':
+        imageQuality = 'q_auto:eco'
+        break
+      case '2g':
       case 'slow-2g':
-        imageQuality = 'q_auto:low';
-        break;
+        imageQuality = 'q_auto:low'
+        break
       default:
-        'q_auto:best';
-        break;
+        'q_auto:best'
+        break
     }
-    const format = 'f_auto';
+    const format = 'f_auto'
 
     console.log(new Date())
-    const imageURLParts = event.request.url.split('/');
-    imageURLParts.splice(imageURLParts.length - 2, 0, `${imageQuality},${format}`);
-    const finalImageURL = new URL(imageURLParts.join('/'));
+    const imageURLParts = event.request.url.split('/')
+    imageURLParts.splice(imageURLParts.length - 2, 0, `${imageQuality},${format}`)
+    const finalImageURL = new URL(imageURLParts.join('/'))
     console.log(finalImageURL)
-    event.respondWith(
-      fetch(finalImageURL.href, { headers: event.request.headers })
-    );
+    event.respondWith(fetch(finalImageURL.href, { headers: event.request.headers }))
   }
-});
+})
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv-safe').config();
+require('dotenv-safe').config()
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.use(express.static('public'))
 
@@ -13,7 +13,7 @@ app.get('/', async (_, res) => {
   res.render('index', pugdata)
 })
 
-const server = app.listen(process.env.PORT, () => { 
-  const port = server.address().port;
-  console.log(`App listening at http://localhost:${port}`);
-});
+const server = app.listen(process.env.PORT, () => {
+  const port = server.address().port
+  console.log(`App listening at http://localhost:${port}`)
+})
